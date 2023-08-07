c06918f7d1f0c971f21261c2a02e9a82d50ec8cb
mduddukuru <mduddukuru@gmail.com>
2023-08-07 19:22:11 +1000

    Task 4.2
    Added a MongoDB Database

--- index.js
+++ index.js
@@ -1,23 +1,68 @@
 let express = require('express');
 let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mduddukuru:Karthik0311___K@cluster0.6zckm3g.mongodb.net/?retryWrites=true&w=majority";
 let port = process.env.port || 3000;
+let collection;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function runDBConnection() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        collection = client.db().collection('Cat');
+        console.log('collection');
+    } catch (ex) {
+        console.error(ex);
+    }
+}
+
 
 app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-app.get('/addtwonumbers', function (req,res) {
-    let number1 = req.query.number1;
-    let number2 = req.query.number2;
-    let result = parseInt(number1) + parseInt(number2);
-    let response = {statusCode: 200, message: 'success', data:result}
-    res.json(response);
-});
 
+app.get('/api/cats', (req,res) => {
+    getAllCats((err, result) => {
+        if (!err) {
+            res.json({statusCode:200, data:result, message: 'get all cats successful'})
+        }
+    });
+})
+
+app.post('/api/cat', (req, res) => {
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (!err){
+            res.json({statusCode:201, data:result, message:'success'});
+        }
+    });
+})
+
+function postCat(cat, callback) {
+    collection.insertOne(cat, callback);
+}
+
+function getAllCats(callback){
+    collection.find({}).toArray(callback);
+}
 
 
 app.listen(port, () => {
     console.log('server started');
+    runDBConnection();
 });
--- scripts.js
+++ scripts.js
@@ -1,24 +1,11 @@
-const cardList = [
-    {
-    title: "Kitten 2",
-    image: "kitten2.jpeg",
-    link: "About Kitten 2",
-    desciption: "Here is Kitten 2, hope you enjoy the pictures!"
-    },
-    {
-    title: "Kitten 3",
-    image: "kitten3.jpeg",
-    link: "About Kitten 3",
-    desciption: "Here is Kitten 3, hope you enjoy the pictures!"
-    }
-    ]
-   
+
+    
     const addCards = (items) => {
     items.forEach(item => {
     let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
     '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.subtitle+'</a></p></div>'+
     '<div class="card-reveal">'+
     '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
     '<p class="card-text black-text">'+item.desciption+'</p>'+
@@ -29,20 +16,44 @@ const cardList = [
     const submitForm = () =>
 {
     let formData = {}
-    formData.firstname = $('#firstname').val()
-    formData.lastname = $('#lastame').val()
-    formData.password = $('#password').val()
-    formData.email = $('#email').val()
+    formData.title = $('#title').val()
+    formData.subtitle = $('#subtitle').val()
+    formData.path = $('#path').val()
+    formData.description = $('#description').val()
+
     console.log("Form Submitted: ", formData);
+    postCat(formData);
+}
+
+function postCat(cat){
+    $.ajax({
+        url: '/api/cat',
+        type: 'POST',
+        data: cat,
+        success: (result) => {
+            if (result.statusCode === 201){
+                alert('cat post successful');
+            }
+        }
+    })
+}
+
+function getAllCats(){
+    $.get('/api/cats', (response) => {
+        if (response.statusCode === 200) {
+            addCards(response.data);
+        }
+        
+    })
 }
 
     $(document).ready(function(){
     $('.materialboxed').materialbox();
-    $('.modal').modal();
-    addCards(cardList);
-    $('#formsubmit').click(()=>{
+    $('#submitForm').click(()=>{
         submitForm();
-    })
+    });
+    $('.modal').modal();
+    getAllCats();
     });
 
 
