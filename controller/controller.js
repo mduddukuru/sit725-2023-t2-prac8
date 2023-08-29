e44b3bef72f399b0fabcfa97edf6c3d269941321
mduddukuru <mduddukuru@gmail.com>
2023-08-29 18:05:34 +1000

    6.2 Testing
    - Added Mocha Chai Request

--- controller/controller.js
+++ controller/controller.js
@@ -18,4 +18,13 @@ const getAllCats = (req,res) => {
     })
 };
 
-module.exports = {postCat, getAllCats};
+const deleteCat = (req,res) => {
+    let cat = req.body;
+    collection.deleteCat(cat, (err, result) => {
+        if (!err) {
+            res.json({statusCode:202, data:result, message: 'deleted'});
+        }
+    });
+}
+
+module.exports = {postCat, getAllCats, deleteCat};
